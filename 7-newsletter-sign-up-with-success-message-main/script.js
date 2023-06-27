055543682a005969fb77efabf2b69974acfe8ca4
TreusK <flutri@protonmail.com>
2023-06-27 20:49:18 -0300

    done

--- 7-newsletter-sign-up-with-success-message-main/script.js
+++ 7-newsletter-sign-up-with-success-message-main/script.js
@@ -1,12 +1,30 @@
 let formButton = document.querySelector('.formButton');
 let formContainer = document.querySelector('.formContainer');
 let successContainer = document.querySelector('.successContainer');
+let form = document.querySelector('form');
+let emailInput = document.querySelector('input');
 
+
+//events
 formButton.addEventListener('click', handleFormBtnClick);
 
+emailInput.addEventListener('focus', function() {
+	emailInput.classList.remove('invalid')
+})
+
+form.addEventListener('submit', function(e) {
+	e.preventDefault();
+})
+
+
 function handleFormBtnClick(e) {
-	formContainer.classList.add('hidden');
-	formContainer.classList.remove('flex');
-	successContainer.classList.add('flex');
-	successContainer.classList.remove('hidden');
+	if(emailInput.checkValidity()) {
+		formContainer.classList.add('hidden');
+		formContainer.classList.remove('flex');
+		successContainer.classList.add('flex');
+		successContainer.classList.remove('hidden');
+	} else {
+		emailInput.classList.add('invalid')
+	}
+	
 }
