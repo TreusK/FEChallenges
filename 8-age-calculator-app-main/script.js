96818a1faffff8f7268518c3e329e5dc9de02b26
TreusK <flutri@protonmail.com>
2023-07-22 18:34:19 -0300

    handling errors

--- 8-age-calculator-app-main/script.js
+++ 8-age-calculator-app-main/script.js
@@ -1,3 +1,4 @@
+//dom grabbin
 let dayInput = document.querySelector('#day');
 let monthInput = document.querySelector('#month');
 let yearInput = document.querySelector('#year');
@@ -6,27 +7,62 @@ let daysRes = document.querySelector('.daysResult');
 let monthsRes = document.querySelector('.monthsResult');
 let yearsRes = document.querySelector('.yearsResult');
 
+let errorDay = document.querySelector('.errorDay');
+let errorMonth = document.querySelector('.errorMonth');
+let errorYear = document.querySelector('.errorYear');
+
 let submitBtn = document.querySelector('.submitBtn')
 
-submitBtn.addEventListener('click', handleAgeForm)
 
+//const
 const month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
-function validateDate(dateString) {
+
+//events
+submitBtn.addEventListener('click', handleAgeForm)
+dayInput.addEventListener('focus', clearError);
+monthInput.addEventListener('focus', clearError);
+yearInput.addEventListener('focus', clearError);
+
+//event functions
+function validateDate(d, m, y) {
+	let valid = true;
+	let dateString = y+ '-' +m+ '-' +d;
+
+	if(d == ''|| m == ''|| y == '') {
+		valid = false;
+	}
+
 	let res = Date.parse(dateString);
-	return (!isNaN(res));
+	if(isNaN(res)) {
+		valid = false;
+	}
+
+	if(d > 31 || d < 1) {
+		errorDay.innerText = "Must be a valid day"
+		valid = false
+	}
+
+	if(m > 12 || m < 1) {
+		errorMonth.innerText = "Must be a valid month"
+	}
+
+	let today = new Date();
+	let yearNow = today.getFullYear();
+	if(y > yearNow || y < 1000) {
+		errorYear.innerText = "Must be a valid year"
+	}
+
+	return valid;
 }
 
 function handleAgeForm() {
 	let d1 = dayInput.value;
 	let m1 = monthInput.value;
 	let y1 = yearInput.value;
-	let dateString = y1+ '-' +m1+ '-' +d1;
-
-	console.log(dateString)
 
 	//if the date isn't valid, stop
-	if(!validateDate(dateString)) {
+	if(!validateDate(d1, m1, y1)) {
 		console.log('bzzt not valid')
 		return
 	}
@@ -48,7 +84,10 @@ function handleAgeForm() {
   daysRes.innerText = d2 - d1;
   monthsRes.innerText = m2 - m1;
   yearsRes.innerText = y2 - y1;
+}
 
- }
 
+function clearError() {
+
+}
   
